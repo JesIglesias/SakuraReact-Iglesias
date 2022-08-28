143d8f903fd3930ac2e54fa505ee24379e821616
Jesica Iglesias <jesicaiglesias@gmail.com>
2022-08-28 18:03:25 -0300

    componente NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+function NavBar() {
+  return (
+    <nav>
+      <h2>Sakura</h2>
+      <ul>
+        <li>
+          <a href="#">Inicio</a>
+        </li>
+        <li>
+          <a href="#">Productos</a>
+        </li>
+        <li>
+          <a href="">Quiénes somos</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
